93e59f714a8bfd41bd738a5850fd32614b7c282c
Andrew Webb <andrew.webb@tuebingen.mpg.de>
2024-10-08 15:46:38 +0200

    tidying up libs directory

--- libs/Accessory/linkfitreal.R
+++ libs/Accessory/linkfitreal.R
@@ -1,15 +1,17 @@
-linkfitreal = function(par, datasub){
-  rewwalksub = datasub[,1:4]
-  trialsub = datasub[,5:6]
-  choicsub = datasub[,7]
- # alpha = par[1]
-  wmb = par[1]
-  wmf = par[2]
- # forget = par[4]
+if (!exists("simufit", mode = "function")) source("libs/Accessory/simufit.R")
+
+linkfitreal <- function(par, datasub) {
+  rewwalksub <- datasub[, 1:4]
+  trialsub <- datasub[, 5:6]
+  choicsub <- datasub[, 7]
+  # alpha = par[1]
+  wmb <- par[1]
+  wmf <- par[2]
+  # forget = par[4]
   # pr = par[5]
   # pz = par[6]
-  #pz = par[5]
-  loglik = simufit(wmb, wmf, rewwalksub, trialsub, choicsub )
-  #loglik = simufit(alpha,wmb, wmf, forget,pr, pz, rewwalksub, trialsub, choicsub )
+  # pz = par[5]
+  loglik <- simufit(wmb, wmf, rewwalksub, trialsub, choicsub)
+  # loglik = simufit(alpha,wmb, wmf, forget,pr, pz, rewwalksub, trialsub, choicsub )
   return(loglik)
 }
